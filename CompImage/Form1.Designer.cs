244e94bb723b4b3fe90b408e16b0ea120faaf7e7
Krzysztof Patelka <49780385+KrzysztofPatelka@users.noreply.github.com>
2023-01-02 10:25:56 +0100

    Add files via upload

---
+++ CompImage/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace CompImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(647, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Wydziel obrazy z PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(629, 394);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(647, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Wczytaj listę plików PDF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(647, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Sprawdź pliki PDF";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 415);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(629, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(644, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Suma: 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(644, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "0 z 0";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "pdf";
+            this.openFileDialog1.Title = "Wczytaj plik";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Wskaż katalog z plikami PDF";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(647, 103);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(141, 303);
+            this.listBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Comparison of images...";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ CompImage/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Pdf;
+
+namespace CompImage
+{
+    public partial class Form1 : Form
+    {
+        public static Task task;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                openFileDialog1.Filter = "plik PDF|*.pdf";
+                openFileDialog1.DefaultExt = "pdf";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    PdfDocument pdf = new PdfDocument();
+                    pdf.LoadFromFile(openFileDialog1.FileName);
+                    int i = 0;
+                    foreach (PdfPageBase page in pdf.Pages)
+                    {
+                        foreach (Image image in page.ExtractImages())
+                        {
+                            i++;
+                            image.Save(Directory.GetCurrentDirectory() + "\\oryginal\\" + Path.GetFileNameWithoutExtension(openFileDialog1.FileName) + "-" + i + ".png", System.Drawing.Imaging.ImageFormat.Png);
+                        }
+                    }
+                    MessageBox.Show("Zapisano " + i + " pliki o nazwach rozpoczynajacyh się od " + Path.GetFileNameWithoutExtension(openFileDialog1.FileName) + "-... w lokalizacji " + Directory.GetCurrentDirectory() + "\\oryginal\\", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                button2.Enabled = false;
+                button3.Enabled = true;
+                listBox1.Items.Clear();
+                if (System.IO.Directory.Exists(folderBrowserDialog1.SelectedPath))
+                {
+                    foreach (string file in System.IO.Directory.GetFiles(folderBrowserDialog1.SelectedPath))
+                    {
+                        //if (Path.GetFileName(file).Substring(0, 1) == "P")
+                        //{
+                        //    listBox1.Items.Add(file);
+                        //}
+                        if (Path.GetExtension(file).ToLower() == ".pdf")
+                        {
+                            listBox1.Items.Add(file);
+                        }
+                    }
+                }
+                label1.Text = "Suma: " + listBox1.Items.Count;
+                label2.Text = "0 z " + listBox1.Items.Count;
+                progressBar1.Maximum = listBox1.Items.Count;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*button2.Enabled = false;
+            button3.Enabled = false;
+            richTextBox1.Clear();
+            string fileError = "";
+            int index = 0;
+            PdfDocument pdf = new PdfDocument();
+            try
+            {
+                foreach (string file in listBox1.Items)
+                {
+                    pdf.LoadFromFile(file);
+                    int i = 1;
+                    foreach (PdfPageBase page in pdf.Pages)
+                    {
+                        foreach (Image image in page.ExtractImages())
+                        {
+                            image.Save(Directory.GetCurrentDirectory() + "\\tmp\\" + Path.GetFileNameWithoutExtension(file) + "_" + i + ".png", System.Drawing.Imaging.ImageFormat.Png);
+                            if (ImageCompareArray(new Bitmap(Directory.GetCurrentDirectory() + "\\oryginal.png"), new Bitmap(Directory.GetCurrentDirectory() + "\\tmp\\" + Path.GetFileNameWithoutExtension(file) + "_" + i + ".png")))
+                            {
+                                //MessageBox.Show("Podpis jest OK", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            else
+                            {
+                                //MessageBox.Show("Podpis jest błędny, sprawdź plik...", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                if (fileError != Path.GetFileName(file))
+                                {
+                                    richTextBox1.AppendText(Environment.NewLine + Path.GetFileName(file));
+                                    fileError = Path.GetFileName(file);
+                                }
+                            }
+                            i += 1;
+                        }
+                    }
+                    index += 1;
+                    progressBar1.Value += 1;
+                    label2.Text = index + " z " + listBox1.Items.Count;
+                }
+                MessageBox.Show("Podpisy zostały sprawdzone.", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }*/
+
+            openFileDialog1.InitialDirectory = Directory.GetCurrentDirectory() + "\\oryginal\\";
+            openFileDialog1.Filter = "plik PNG|*.png";
+            openFileDialog1.DefaultExt = "png";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                listBox2.Items.Clear();
+                Task.Run(() =>
+                {
+                    string fileError = "";
+                    int index = 0;
+                    PdfDocument pdf = new PdfDocument();
+                    try
+                    {
+                        foreach (string file in listBox1.Items)
+                        {
+                            pdf.LoadFromFile(file);
+                            int i = 1;
+                            foreach (PdfPageBase page in pdf.Pages)
+                            {
+                                foreach (Image image in page.ExtractImages())
+                                {
+                                    if (ImageCompareArray(new Bitmap(openFileDialog1.FileName), new Bitmap(image)))
+                                    {
+                                        //MessageBox.Show("Podpis jest OK", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    }
+                                    else
+                                    {
+                                        if (fileError != Path.GetFileName(file))
+                                        {
+                                            Invoke(new Action(delegate () {
+                                                listBox2.Items.Add(Path.GetFileName(file));
+                                            }));
+                                            fileError = Path.GetFileName(file);
+                                        }
+                                    }
+                                    i += 1;
+                                }
+                            }
+                            index += 1;
+                            Invoke(new Action(delegate () {
+                                progressBar1.Value += 1;
+                                label2.Text = index + " z " + listBox1.Items.Count;
+                            }));
+                        }
+                        MessageBox.Show("Podpisy zostały sprawdzone.", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                });
+            }
+        }
+
+        private bool ImageCompareArray(Bitmap firstImage, Bitmap secondImage)
+        {
+            bool flag = true;
+            if (firstImage.Width == secondImage.Width && firstImage.Height == secondImage.Height)
+            {
+                for (int i = 0; i < firstImage.Width; i++)
+                {
+                    for (int j = 0; j < firstImage.Height; j++)
+                    {
+                        if (firstImage.GetPixel(i, j).ToString() != secondImage.GetPixel(i, j).ToString())
+                        {
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+                if (flag == false)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
